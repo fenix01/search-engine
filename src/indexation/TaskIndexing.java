6e9df2d7a468fc2e401373326de34512ebd7f81c
pierre barussaud <pierre.barussaud@u-psud.fr>
2014-11-03 16:49:30 +0100

    supression dans l'index des ligne ayant un df = 1

--- src/indexation/TaskIndexing.java
+++ src/indexation/TaskIndexing.java
@@ -20,15 +20,15 @@ import td.td3.TD3;
 import tools.Normalizer;
 
 public class TaskIndexing implements Runnable {
-	
+
 	private int start_doc = 0;
 	private int end_doc = 0;
 	private HashMap<Integer, String[]> corpus;
-	
-	//on stocke l'index sous la forme d'un treemap de
-	//String = mot en clé, et un HashMap<DocID,tf> en valeur
-	//2 entiers sont plus légers en mémoire qu'une string de DocID:Tf
-	private TreeMap<String, HashMap<Integer,Integer>> index;
+
+	// on stocke l'index sous la forme d'un treemap de
+	// String = mot en clé, et un HashMap<DocID,tf> en valeur
+	// 2 entiers sont plus légers en mémoire qu'une string de DocID:Tf
+	private TreeMap<String, HashMap<Integer, Integer>> index;
 	private Normalizer normalizer;
 	private boolean stopwords;
 	private LinkedList<String> tmp_idx;
@@ -39,79 +39,90 @@ public class TaskIndexing implements Runnable {
 
 	/**
 	 * permet de fusionner les indexes finaux générer par les threads
+	 * 
 	 * @param threads_count
 	 */
-	public static void fusionThreadsIndexes(int threads_count){
+	public static void fusionThreadsIndexes(int threads_count) {
 		LinkedList<String> threadsIndex = new LinkedList<>();
-		for (int i = 0; i < threads_count ; i++){
-			String indexName =  Common.DIRINDEX+"index"+i+Common.extIDX;
+		for (int i = 0; i < threads_count; i++) {
+			String indexName = Common.DIRINDEX + "index" + i + Common.extIDX;
 			threadsIndex.add(indexName);
 		}
 		fusionIndexes(threadsIndex, "index");
 	}
-	
+
 	/**
-	 * retourne les premières lettres d'un mot de la ligne d'index
-	 * vérifie si certains mots ont une taille inférieur à x
-	 * @param line 
+	 * retourne les premières lettres d'un mot de la ligne d'index vérifie si
+	 * certains mots ont une taille inférieur à x
+	 * 
+	 * @param line
 	 * @param x
 	 * @return
 	 */
-	private static String firstOcc(String line, int x){
+	private static String firstOcc(String line, int x) {
 		if (line == null)
 			return null;
 		String word = line.split("\t")[0];
 		if (word.length() < x)
 			return word;
 		else
-			return word.substring(0,x);
+			return word.substring(0, x);
 	}
-	
+
 	/**
 	 * permet de découper l'index selon les X premières lettres des mots
+	 * 
 	 * @param x
 	 */
-	public static void splitIndex(int x){
+	public static void splitIndex(int x) {
 		FileReader fr;
 		FileWriter fw;
 		try {
-			File fIndex = new File(Common.DIRINDEX+"index"+Common.extIDX);
+			File fIndex = new File(Common.DIRINDEX + "index" + Common.extIDX);
 			fr = new FileReader(fIndex);
 			BufferedReader br = new BufferedReader(fr);
-			
+
 			String line1 = br.readLine();
 			String line2 = br.readLine();
-			
+
 			String firstOccLine1;
 			String firstOccLine2;
-			firstOccLine1 = firstOcc(line1,x);
-			firstOccLine2 = firstOcc(line2,x);
-			fw = new FileWriter(Common.DIRINDEX+firstOccLine1+Common.extIDX,true);
+			firstOccLine1 = firstOcc(line1, x);
+			firstOccLine2 = firstOcc(line2, x);
+			fw = new FileWriter(
+					Common.DIRINDEX + firstOccLine1 + Common.extIDX, true);
 			BufferedWriter bw = new BufferedWriter(fw);
-			while (line2 != null){
-				if (firstOccLine1.equals(firstOccLine2)){
-					bw.write(line1);
-					bw.newLine();
-				}
-				else {
-					bw.write(line1);
-					bw.newLine();
+			while (line2 != null) {
+				if (firstOccLine1.equals(firstOccLine2)) {
+					if (Integer.parseInt(line1.split("\t")[1])>1) {
+						bw.write(line1);
+						bw.newLine();
+					}
+				} else {
+
+					if (Integer.parseInt(line1.split("\t")[1])>1) {
+						bw.write(line1);
+						bw.newLine();
+						bw.close();
+					}
 					bw.close();
-					fw = new FileWriter(Common.DIRINDEX+firstOccLine2+Common.extIDX,true);
+					fw = new FileWriter(Common.DIRINDEX + firstOccLine2
+							+ Common.extIDX, true);
 					bw = new BufferedWriter(fw);
 				}
+
 				line1 = line2;
 				line2 = br.readLine();
-				
-				firstOccLine1 = firstOcc(line1,x);
-				firstOccLine2 = firstOcc(line2,x);
+
+				firstOccLine1 = firstOcc(line1, x);
+				firstOccLine2 = firstOcc(line2, x);
 			}
 			bw.write(line1);
 			bw.newLine();
 			bw.close();
 			br.close();
-			fIndex.delete();
-			
+			//fIndex.delete();
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -129,15 +140,16 @@ public class TaskIndexing implements Runnable {
 			try {
 				// System.out.println(i);
 				HashMap<String, Integer> tf_doc;
-				
+
 				String doc;
 				doc = corpus.get(i)[0];
 
 				tf_doc = TD2.getTermFrequencies(doc, normalizer, stopwords);
 				for (Map.Entry<String, Integer> word : tf_doc.entrySet()) {
-					
+
 					if (!Common.isEmptyWord(word.getKey())) {
-						HashMap<Integer, Integer> listDocs = index.get(word.getKey());
+						HashMap<Integer, Integer> listDocs = index.get(word
+								.getKey());
 						if (listDocs != null) {
 							// le mot existe dans l'index, on l'ajoute dans la
 							// liste des documents
@@ -149,33 +161,34 @@ public class TaskIndexing implements Runnable {
 							index.put(word.getKey(), list_docs);
 						}
 					}
-				}	
-				
+				}
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-//			if(Utils.getUsedMemory()>=1000000000){
-//			System.out.println(i);
-//			System.out.println(Utils.memoryInfo());
-//			saveTempIndex();
-//			}
-			if (compteur == this.reached){
-					System.out.println(Utils.memoryInfo());
-					compteur = 0;
-					saveTempIndex();
+			// if(Utils.getUsedMemory()>=1000000000){
+			// System.out.println(i);
+			// System.out.println(Utils.memoryInfo());
+			// saveTempIndex();
+			// }
+			if (compteur == this.reached) {
+				System.out.println(Utils.memoryInfo());
+				compteur = 0;
+				saveTempIndex();
 			}
 			compteur++;
 		}
 		saveTempIndex();
-		fusionIndexes(this.tmp_idx,this.name_idx);
+		fusionIndexes(this.tmp_idx, this.name_idx);
 		long estimatedTime = System.currentTimeMillis() - startTime;
 		System.out.println(estimatedTime / 1000);
 	}
-	
-	private void saveTempIndex(){
+
+	private void saveTempIndex() {
 		try {
-			String out_idx = Common.DIRINDEX+this.name_idx+this.cur_index+Common.extIDX;
+			String out_idx = Common.DIRINDEX + this.name_idx + this.cur_index
+					+ Common.extIDX;
 			this.tmp_idx.add(out_idx);
 			TD3.saveInvertedFile(index, new File(out_idx));
 			this.index.clear();
@@ -186,29 +199,36 @@ public class TaskIndexing implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * permet de fusionner tous les indexes temporaires de la LinkedList passer en paramètre
-	 * en un seul fichier d'index. Chaque entrée de la LinkedList est un chemin vers un index temporaire
+	 * permet de fusionner tous les indexes temporaires de la LinkedList passer
+	 * en paramètre en un seul fichier d'index. Chaque entrée de la LinkedList
+	 * est un chemin vers un index temporaire
 	 * 
-	 * @param tmpIndexes est un LinkedList contenant des chemins vers des indexes temporaires
-	 * @param nameIndex correspond au prefix pour les noms des fichiers d'index temporaire
+	 * @param tmpIndexes
+	 *            est un LinkedList contenant des chemins vers des indexes
+	 *            temporaires
+	 * @param nameIndex
+	 *            correspond au prefix pour les noms des fichiers d'index
+	 *            temporaire
 	 */
-	private static void fusionIndexes(LinkedList<String> tmpIndexes, String nameIndex){
-		//compteur pour gérer les indexes temporaires
+	private static void fusionIndexes(LinkedList<String> tmpIndexes,
+			String nameIndex) {
+		// compteur pour gérer les indexes temporaires
 		int counter = tmpIndexes.size();
-		//tant qu'il reste au moins 2 indexes temporaires
-		while (tmpIndexes.size() > 1){
-			//on dépile 2 indexes temporaires
+		// tant qu'il reste au moins 2 indexes temporaires
+		while (tmpIndexes.size() > 1) {
+			// on dépile 2 indexes temporaires
 			String outIndex1 = tmpIndexes.pollFirst();
 			String outIndex2 = tmpIndexes.pollFirst();
-			//on prépare les deux indexes
+			// on prépare les deux indexes
 			File f1 = new File(outIndex1);
 			File f2 = new File(outIndex2);
-			//on prépare un nouvelle index temporaire fusion de f1 et f2
-			String outIndexMerge = Common.DIRINDEX+nameIndex+counter+Common.extIDX;
+			// on prépare un nouvelle index temporaire fusion de f1 et f2
+			String outIndexMerge = Common.DIRINDEX + nameIndex + counter
+					+ Common.extIDX;
 			counter++;
-			//on fusionne les deux indexes temporaires
+			// on fusionne les deux indexes temporaires
 			File fMerge = new File(outIndexMerge);
 			try {
 				FusionIndex.mergeInvertedFiles(f1, f2, fMerge);
@@ -216,24 +236,26 @@ public class TaskIndexing implements Runnable {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//on ajoute le nouvel index fusionné à la fin de la liste des indexes temporaires
+			// on ajoute le nouvel index fusionné à la fin de la liste des
+			// indexes temporaires
 			tmpIndexes.addLast(outIndexMerge);
-			//on supprime les 2 fichiers d'index temporaire
+			// on supprime les 2 fichiers d'index temporaire
 			f1.delete();
 			f2.delete();
 		}
-		//on renomme à présent l'index temporaire résultat de la fusion de tous les indexes temporaires
+		// on renomme à présent l'index temporaire résultat de la fusion de tous
+		// les indexes temporaires
 		String tmpIndex = tmpIndexes.poll();
 		File f = new File(tmpIndex);
-		String outIndex = Common.DIRINDEX+nameIndex+Common.extIDX;
+		String outIndex = Common.DIRINDEX + nameIndex + Common.extIDX;
 		f.renameTo(new File(outIndex));
 	}
 
 	public void start() {
 		th.start();
 	}
-	
-	public void join(){
+
+	public void join() {
 		try {
 			th.join();
 		} catch (InterruptedException e) {
